66e18e8bed9d09ea9bfb03cf8e13897833a9ee30
Hadi Hleihel <hadi.hleihel@student.howest.be>
2026-03-13 09:12:43 +0000

    feat: Implement Birds CRUD with Species relationship

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from database import start_db
-from routers import species
+from routers import birds, species
 
 app = FastAPI(
     title="Birds API",
@@ -10,6 +10,7 @@ app = FastAPI(
 )
 
 app.include_router(species.router)
+app.include_router(birds.router)
 
 
 @app.on_event("startup")
